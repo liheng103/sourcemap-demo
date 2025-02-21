c320df0c9f495afc5374cb9f0457717589315ced
liheng103 <1353155127@qq.com>
2025-02-21 22:48:24 +0800

    github page 部署

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ app.config.errorHandler = (error: any, vm) => {
     error_name: error.name,
   }
   console.log('jsError', jsError)
-  //   vm!.$message.error(`您触发了一个${error.name} 错误`)
+  vm!.$message.error(`您触发了一个${error.name} 错误`)
   localStorage.setItem('jsErrorList', JSON.stringify(jsError))
   //   findCodeBySourceMap(errorStack[0])
 }
